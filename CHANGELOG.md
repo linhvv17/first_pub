bf9569f9da66f85b90ffdc14718b5ce1ba74cbf7
linhvv17 <linhvv17@gmail.com>
2023-03-26 02:05:00 +0700

    my change

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+0.0.1
+Added the container files with modifiable title, subtitle, height, width and more properties.
---
+++ android/app/src/main/kotlin/com/example/first_my_pub/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.first_my_pub
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
